f4fefc5a6ec5b82670ca41cd7aae2385e1e0ee87
Newbe <472158246@qq.com>
2017-05-30 15:50:19 +0800

    注释

--- Newbe.Mahua.Framework/IPluginInfo.cs
+++ Newbe.Mahua.Framework/IPluginInfo.cs
@@ -1,10 +1,28 @@
 ﻿namespace Newbe.Mahua.Framework
 {
+    /// <summary>
+    /// 插件信息
+    /// </summary>
     public interface IPluginInfo
     {
+        /// <summary>
+        /// 版本
+        /// </summary>
         string Version { get; set; }
+
+        /// <summary>
+        /// 名称
+        /// </summary>
         string Name { get; set; }
+
+        /// <summary>
+        /// Id，插件唯一标识
+        /// </summary>
         string Id { get; set; }
+
+        /// <summary>
+        /// 插件描述信息
+        /// </summary>
         string Description { get; set; }
     }
 }
