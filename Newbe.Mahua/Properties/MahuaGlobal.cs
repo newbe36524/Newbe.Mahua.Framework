c1a1a814119048caf302a96ae8f1c0884eccbd5e
Newbe <472158246@qq.com>
2017-06-04 20:30:29 +0800

    当前插件平台

---
+++ Newbe.Mahua/Properties/MahuaGlobal.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newbe.Mahua.Properties
+{
+    /// <summary>
+    /// 全局信息
+    /// </summary>
+    public static class MahuaGlobal
+    {
+        // ReSharper disable once InconsistentNaming
+        private static readonly Lazy<MahuaPlatform> currentPlatform =
+            new Lazy<MahuaPlatform>(() =>
+            {
+                return new IPlatformResolver[]
+                        {new CqpPlatformResolver(), new MpqPlatformResolver(), new AmandaPlatformResolver()}
+                    .First(x => x.IsThis()).MahuaPlatform;
+            });
+
+        /// <summary>
+        /// 当前机器人平台
+        /// </summary>
+        public static MahuaPlatform CurrentPlatform => currentPlatform.Value;
+
+        private static string GetCurrentDir() => Environment.CurrentDirectory;
+
+        #region IPlatformResolver
+
+        internal interface IPlatformResolver
+        {
+            MahuaPlatform MahuaPlatform { get; }
+            bool IsThis();
+        }
+
+        class CqpPlatformResolver : IPlatformResolver
+        {
+            public MahuaPlatform MahuaPlatform { get; } = MahuaPlatform.Cqp;
+
+            public bool IsThis()
+            {
+                var currentDir = GetCurrentDir();
+                return File.Exists(Path.Combine(currentDir, "CQA.exe")) ||
+                       File.Exists(Path.Combine(currentDir, "CQP.exe"));
+            }
+        }
+
+        class MpqPlatformResolver : IPlatformResolver
+        {
+            public MahuaPlatform MahuaPlatform { get; } = MahuaPlatform.Mpq;
+
+            public bool IsThis()
+            {
+                var currentDir = GetCurrentDir();
+                return File.Exists(Path.Combine(currentDir, "Core.exe"));
+            }
+        }
+
+        class AmandaPlatformResolver : IPlatformResolver
+        {
+            public MahuaPlatform MahuaPlatform { get; } = MahuaPlatform.Amanda;
+
+            public bool IsThis()
+            {
+                var currentDir = GetCurrentDir();
+                return File.Exists(Path.Combine(currentDir, "Amanda.exe"));
+            }
+        }
+
+        #endregion
+    }
+}
