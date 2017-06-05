243ef5bdd0bb372268d6c33cc784a43a86d90e25
Newbe <472158246@qq.com>
2017-06-05 23:36:08 +0800

    nuget pack task

--- build.ps1
+++ build.ps1
@@ -1,13 +1,21 @@
 Framework "4.6"
+Properties {
+    $nugetPackVersion = "1.0.0"
+}
 
 Task Default -depends CI 
 
-Task CI -depends Build{
+Task CI -depends NugetPack -Desciption "持续集成" {
 
 }
-Task Nuget {
+Task Nuget -Desciption "Nuget还原" {
     .nuget/nuget.exe restore
 }
-Task Build -depends Nuget{
-    msbuild 
+Task Build -depends Nuget -Desciption "编译解决方案" {
+    msbuild /p:Configuration=Release
+}
+Task NugetPack -depends Build -Desciption "生成Nuget包" {
+    Get-ChildItem Nuspecs *.nuspec -File | ForEach-Object {
+        .nuget/nuget.exe pack $_.FullName -Version $nugetPackVersion -OutputDirectory npks
+    }
 }
