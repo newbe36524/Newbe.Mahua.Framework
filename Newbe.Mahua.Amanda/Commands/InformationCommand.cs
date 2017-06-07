591756ab425155aa8737c518cf467a6772d6f7fc
Newbe <472158246@qq.com>
2017-06-07 23:51:03 +0800

    info

--- Newbe.Mahua.Amanda/Commands/InformationCommand.cs
+++ Newbe.Mahua.Amanda/Commands/InformationCommand.cs
@@ -3,11 +3,29 @@ using Newbe.Mahua.Commands;
 
 namespace Newbe.Mahua.Amanda.Commands
 {
-    internal class InformationCommandHandler : CommandHandlerBase<InformationCommand>
+    internal class InformationCommandHandler : CommandHandlerBase<InformationCommand, InformationCommandResult>
     {
-        protected override void HandleCore(InformationCommand command)
+        private static readonly string SdkVersion = "1";
+        private readonly IPluginInfo _pluginInfo;
+
+        public InformationCommandHandler(IPluginInfo pluginInfo)
         {
-            throw new System.NotImplementedException();
+            _pluginInfo = pluginInfo;
+        }
+
+        protected override InformationCommandResult HandleCore(InformationCommand command)
+        {
+            var info = $"pluginID={_pluginInfo.Id};{Environment.NewLine}" +
+                       $"pluginName={_pluginInfo.Name};{Environment.NewLine}" +
+                       $"pluginBrief=;{Environment.NewLine}" +
+                       $"pluginVersion={_pluginInfo.Version};{Environment.NewLine}" +
+                       $"pluginSDK={SdkVersion};{Environment.NewLine}" +
+                       $"pluginAuthor={_pluginInfo.Author};{Environment.NewLine}" +
+                       $"pluginWindowsTitle={{_TestMenu1=测试窗口1}}{{_TestMenu2=测试窗口2}};";
+            return new InformationCommandResult
+            {
+                Info = info
+            };
         }
     }
 
