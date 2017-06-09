5e8d12e7eab0f4ad42451b905b9a0b5e8e60927c
Newbe <472158246@qq.com>
2017-06-09 23:11:45 +0800

    NotSupportMahuaPlatformExpcetion

--- Newbe.Mahua/MahuaGlobal.cs
+++ Newbe.Mahua/MahuaGlobal.cs
@@ -14,9 +14,10 @@ namespace Newbe.Mahua
         public static MahuaPlatform CurrentPlatform => MahuaPlatformValueProvider.CurrentPlatform.Value;
 
         /// <summary>
-        /// 调用标记为<see cref="NotSupportedMahuaApiAttribute"/>的Api的行为。默认为<see cref="ThrowsNotSupportedMahuaApiConvertion{NotImplementedException}"/>
+        /// 调用标记为<see cref="NotSupportedMahuaApiAttribute"/>的Api的行为。默认为<see cref="ThrowsNotSupportedMahuaApiConvertion{NotSupportMahuaPlatformExpcetion}"/>
         /// </summary>
         public static INotSupportedMahuaApiConvertion NotSupportedMahuaApiConvertion { get; set; } =
-            MahuaConvertions.NotSupportedMahuaApiConvertions.IgnoreNotSupportedMahuaApiConvertion;
+            MahuaConvertions.NotSupportedMahuaApiConvertions
+                .GetThrowsNotSupportedMahuaApiConvertion<NotSupportMahuaPlatformExpcetion>();
     }
 }
