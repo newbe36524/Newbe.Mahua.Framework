5b8659e0dcb3c4b53ee0dfb0825c7e1b9dc3a124
Newbe <472158246@qq.com>
2017-06-09 21:08:20 +0800

    IInitializationMahuaEvent

--- Newbe.Mahua.CQP/Commands/InitializeCommand.cs
+++ Newbe.Mahua.CQP/Commands/InitializeCommand.cs
@@ -1,21 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 using Newbe.Mahua.Commands;
 using Newbe.Mahua.CQP.NativeApi;
+using Newbe.Mahua.MahuaEvents;
 
 namespace Newbe.Mahua.CQP.Commands
 {
     internal class InitializeCommandHandler : CommandHandlerBase<InitializeCommand>
     {
         private readonly ICqpAuthCodeContainer _cqpAuthCodeContainer;
+        private readonly IEnumerable<IInitializationMahuaEvent> _initializationMahuaEvents;
 
-        public InitializeCommandHandler(ICqpAuthCodeContainer cqpAuthCodeContainer)
+        public InitializeCommandHandler(ICqpAuthCodeContainer cqpAuthCodeContainer,
+            IEnumerable<IInitializationMahuaEvent> initializationMahuaEvents)
         {
             _cqpAuthCodeContainer = cqpAuthCodeContainer;
+            _initializationMahuaEvents = initializationMahuaEvents;
         }
 
         protected override void HandleCore(InitializeCommand command)
         {
             _cqpAuthCodeContainer.AuthCode = command.AuthCode;
+            _initializationMahuaEvents.Handle(x => x.Initialized(new InitializedContext()));
         }
     }
 
---
+++ Newbe.Mahua/MahuaEvents/IInitializationMahuaEvent.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace Newbe.Mahua.MahuaEvents
+{
+    /// <summary>
+    /// 插件初始化事件
+    /// </summary>
+    [Description("插件初始化事件")]
+    public interface IInitializationMahuaEvent : IMahuaEvent
+    {
+        /// <summary>
+        /// 初始化插件
+        /// </summary>
+        /// <param name="context"></param>
+        void Initialized(InitializedContext context);
+    }
+
+    /// <summary>
+    /// 初始化插件
+    /// </summary>
+    public class InitializedContext
+    {
+    }
+}
