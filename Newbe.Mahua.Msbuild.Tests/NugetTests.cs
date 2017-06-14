9ed2946a9c5d00fe0d7d0366faba3297c592b536
Newbe <472158246@qq.com>
2017-06-14 23:38:42 +0800

    修复msbuild组件在选取插件实现版本时异常的问题 (#9)
    * test
    
    * bug fix 1.0.1.4
    
    * nuget pack changed
    
    * 集成测试

---
+++ Newbe.Mahua.Msbuild.Tests/NugetTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using FluentAssertions;
+using NuGet;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Newbe.Mahua.Msbuild.Tests
+{
+    public class NugetTests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public NugetTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        //集成测试[Fact]
+        public void GetNewestVersion()
+        {
+            var npk = NugetHelper.GetLastestVersionPackage(Path.GetFullPath("../../pkgs"), "Package");
+            npk.Version.ToString().Should().Be("3.0.0-pre");
+            _testOutputHelper.WriteLine(npk.Version.ToString());
+        }
+
+
+        //集成测试[Fact]
+        public void IntergationTest()
+        {
+            var npk = NugetHelper.GetLastestVersionPackage(@"D:\Codes\Repos\Newbe.Mahua.Framework\packages",
+                "Newbe.Mahua.CQP");
+            npk.Version.ToFullString().Should().Be("1.0.1.2");
+            _testOutputHelper.WriteLine(npk.Version.ToString());
+        }
+    }
+}
---
+++ Newbe.Mahua.Msbuild/NugetHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Linq;
+using NuGet;
+
+namespace Newbe.Mahua.Msbuild
+{
+    internal static class NugetHelper
+    {
+        internal static IPackage GetLastestVersionPackage(string packageDir, string packageId)
+        {
+            var repo = new LocalPackageRepository(Path.GetFullPath(packageDir));
+            var pkgs = repo.GetPackages().ToArray();
+            var targetPackages = pkgs.Where(x => x.Id == packageId).ToArray();
+            var newbePk =
+                targetPackages.First(x => x.Id == packageId && x.Version == targetPackages.Max(a => a.Version));
+            return newbePk;
+        }
+    }
+}
