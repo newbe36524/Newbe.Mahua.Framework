96049126cab25288be40bbeb966b5be26b9bf18f
Newbe <472158246@qq.com>
2017-06-27 22:54:41 +0800

    删除nuget中对旧文库的操作

--- Nuspecs/install.ps1
+++ Nuspecs/install.ps1
@@ -1,6 +1,4 @@
 ﻿param($installPath, $toolsPath, $package, $project)
 Start-Process "http://www.newbe.cf/"
-Start-Process "http://www.newbe.cf/Mahua/docs/index.html"
 Start-Process "https://jq.qq.com/?_wv=1027&k=4AMMCTx"
 Start-Process "http://git.oschina.net/yks/Newbe.Mahua.Framework"
-
