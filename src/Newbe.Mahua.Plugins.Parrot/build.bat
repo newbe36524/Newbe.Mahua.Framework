f7656e2479809cf9f198400c1f4161e87f3a809f
Newbe <472158246@qq.com>
2017-10-26 21:50:29 +0800

    1.2.0

---
+++ src/Newbe.Mahua.Plugins.Parrot/build.bat
@@ -0,0 +1,3 @@
+
+..\packages\NuGet.CommandLine.4.3.0\tools\NuGet.exe install buildTools\packages.config -OutputDirectory ..\packages -Verbosity normal
+powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "& {Import-Module '..\packages\psake.*\tools\psake.psm1';Import-Module '..\packages\Newbe.Build.Psake.*\tools\VSSetup\**\*.psd1';Import-Module '..\packages\Newbe.Build.Psake.*\tools\*.psm1'; invoke-psake .\build.ps1 %*; if ($LastExitCode -and $LastExitCode -ne 0) {write-host "ERROR CODE: $LastExitCode" -fore RED; exit $lastexitcode} }"
