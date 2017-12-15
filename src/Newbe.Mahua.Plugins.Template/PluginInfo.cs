630ec0d26b47df003d41df434acda5b848635e00
Newbe <472158246@qq.com>
2017-12-15 22:21:25 +0800

    templates

---
+++ src/Newbe.Mahua.Plugins.Template/PluginInfo.cs
@@ -0,0 +1,34 @@
+﻿namespace Newbe.Mahua.Plugins.Template
+{
+    /// <summary>
+    /// 本插件的基本信息
+    /// </summary>
+    public class PluginInfo : IPluginInfo
+    {
+        /// <summary>
+        /// 版本号，建议采用 主版本.次版本.修订号 的形式
+        /// </summary>
+        public string Version { get; set; } = "1.0.0";
+
+        /// <summary>
+        /// 插件名称
+        /// </summary>
+
+        public string Name { get; set; } = "插件名称";
+
+        /// <summary>
+        /// 作者名称
+        /// </summary>
+        public string Author { get; set; } = "Newbe";
+
+        /// <summary>
+        /// 插件Id，用于唯一标识插件产品的Id，至少包含 AAA.BBB.CCC 三个部分
+        /// </summary>
+        public string Id { get; set; } = "Newbe.Mahua.Plugins.Template";
+
+        /// <summary>
+        /// 插件描述
+        /// </summary>
+        public string Description { get; set; } = "插件描述";
+    }
+}
---
+++ src/Newbe.Mahua.Plugins.Template/build.ps1
@@ -0,0 +1,73 @@
+﻿Framework "4.6"
+properties {
+    $rootNow = Resolve-Path .
+    $nugetexe = "$rootNow\..\packages\NuGet.CommandLine.4.3.0\tools\NuGet.exe"
+    $configuration = "Release"
+    $releaseBase = "$rootNow\bin"
+    $pluginName = (Get-ChildItem *.csproj).Name.Replace(".csproj", "")
+}
+
+$InstalledPlatforms = Get-ChildItem NewbeLibs\Platform
+
+Task Default -depends Pack
+
+Task Clean -Description "清理" {
+    Write-Output $InstalledPlatforms
+    $InstalledPlatforms | ForEach-Object {
+        Exec {
+            Remove-Item "$releaseBase\$_" -Recurse -Force -ErrorAction SilentlyContinue
+        }
+    }
+}
+
+Task Init -depends Clean -Description "初始化参数" {
+
+}
+
+Task Nuget -depends Init -Description "nuget restore" {
+    Exec {
+        cmd /c ""$nugetexe" restore  -PackagesDirectory ..\packages"
+    }
+}
+
+Task Build -depends Nuget -Description "编译" {
+    Exec {
+        msbuild /p:Configuration=$configuration
+    }
+}
+Task PackCQP -depends Build -Description "CQP打包" {
+    if ($InstalledPlatforms | Where-Object {$_.Name -eq "CQP"}) {
+        New-Item -ItemType Directory "$releaseBase\CQP"
+        New-Item -ItemType Directory "$releaseBase\CQP\$pluginName"
+        New-Item -ItemType Directory "$releaseBase\CQP\app"
+        Copy-Item -Path  ".\NewbeLibs\Framework\*", ".\NewbeLibs\Platform\CQP\CLR\*" -Destination "$releaseBase\CQP" -Recurse
+        Copy-Item -Path "$releaseBase\$configuration\*", ".\NewbeLibs\Platform\CQP\CLR\*"   -Destination "$releaseBase\CQP\$pluginName" -Recurse
+        Copy-Item -Path ".\NewbeLibs\Platform\CQP\Native\Newbe.Mahua.CQP.Native.dll" -Destination  "$releaseBase\CQP\app\$pluginName.dll"
+        Copy-Item -Path ".\NewbeLibs\Platform\CQP\Content\Newbe.Mahua.CQP.json" -Destination  "$releaseBase\CQP\app\$pluginName.json"
+    }
+}
+
+Task PackAmanda -depends Build -Description "Amanda打包" {
+    if ($InstalledPlatforms | Where-Object {$_.Name -eq "Amanda"}) {
+        New-Item -ItemType Directory "$releaseBase\Amanda"
+        New-Item -ItemType Directory "$releaseBase\Amanda\$pluginName"
+        New-Item -ItemType Directory "$releaseBase\Amanda\plugin"
+        Copy-Item -Path  ".\NewbeLibs\Framework\*", ".\NewbeLibs\Platform\Amanda\CLR\*" -Destination "$releaseBase\Amanda" -Recurse
+        Copy-Item -Path "$releaseBase\$configuration\*", ".\NewbeLibs\Platform\Amanda\CLR\*"   -Destination "$releaseBase\Amanda\$pluginName" -Recurse
+        Copy-Item -Path ".\NewbeLibs\Platform\Amanda\Native\Newbe.Mahua.Amanda.Native.dll" -Destination  "$releaseBase\Amanda\plugin\$pluginName.plugin.dll"
+    }
+}
+
+Task PackMPQ -depends Build -Description "MPQ打包" {
+    if ($InstalledPlatforms | Where-Object {$_.Name -eq "MPQ"}) {
+        New-Item -ItemType Directory "$releaseBase\MPQ"
+        New-Item -ItemType Directory "$releaseBase\MPQ\$pluginName"
+        New-Item -ItemType Directory "$releaseBase\MPQ\Plugin"
+        Copy-Item -Path  ".\NewbeLibs\Framework\*", ".\NewbeLibs\Platform\MPQ\CLR\*" -Destination "$releaseBase\MPQ" -Recurse
+        Copy-Item -Path "$releaseBase\$configuration\*", ".\NewbeLibs\Platform\MPQ\CLR\*"   -Destination "$releaseBase\MPQ\$pluginName" -Recurse
+        Copy-Item -Path ".\NewbeLibs\Platform\MPQ\Native\Newbe.Mahua.MPQ.Native.dll" -Destination  "$releaseBase\MPQ\Plugin\$pluginName.xx.dll"
+    }
+}
+
+Task Pack -depends PackCQP,PackAmanda,PackMPQ -Description "打包"
+
