5b1a10d2ede0855e0a166e3b01d7cfab4e82d0fb
Newbe <472158246@qq.com>
2017-12-23 15:05:15 +0800

    MenuEvent and item

---
+++ src/Newbe.Mahua/MahuaEvents/IManhuaMenuClickedMahuaEvent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Newbe.Mahua.MahuaEvents
+{
+    /// <summary>
+    /// 菜单点击事件
+    /// </summary>
+    [Description("菜单点击事件")]
+    public interface IManhuaMenuClickedMahuaEvent : IMahuaEvent
+    {
+        /// <summary>
+        /// 处理事件
+        /// </summary>
+        /// <param name="context"></param>
+        void ProcessManhuaMenuClicked(ManhuaMenuClickedContext context);
+    }
+
+    /// <summary>
+    /// 事件上下文
+    /// </summary>
+    public class ManhuaMenuClickedContext
+    {
+        /// <summary>
+        /// 被点击的菜单
+        /// </summary>
+        public MahuaMenu Menu { get; set; }
+
+        /// <summary>
+        /// 点击时间
+        /// </summary>
+        public DateTime ClickedTime { get; set; }
+    }
+}
