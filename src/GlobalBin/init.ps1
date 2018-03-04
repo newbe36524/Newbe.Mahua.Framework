44ccf2622b878acc451397885c73e4196bae885d
Newbe <472158246@qq.com>
2018-03-04 21:32:54 +0800

    update www.newbe.cf to www.newbe.pro

--- src/GlobalBin/init.ps1
+++ src/GlobalBin/init.ps1
@@ -3,7 +3,7 @@
 $pcName = (Get-WmiObject -Class Win32_UserAccount -Filter "Name='$env:username' and Domain='$env:userdomain'").Name
 
 if($pcName -ne "yks"){
-	Start-Process "http://www.newbe.cf/"
+	Start-Process "http://www.newbe.pro/"
 	Start-Process "https://gitee.com/yks/Newbe.Mahua.Framework#project-donate-overview"
 }
 
