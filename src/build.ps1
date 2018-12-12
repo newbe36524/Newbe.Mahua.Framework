0b8c7c1fb4a99657e0de29afe2a791a224be4866
newbe36524 <472158246@qq.com>
2018-12-12 19:55:49 +0800

    使用 Newbe.Build.Psake 2.0 构建项目源码

--- src/build.ps1
+++ src/build.ps1
@@ -1,7 +1,6 @@
-﻿Framework "4.6"
+﻿Framework 4.6
 properties {
     $rootNow = Resolve-Path .
-    $nugetexe = "$rootNow\buildTools\NuGet.exe"
     $deployMode = "Debug"
     $releaseDir = "$rootNow\build\"
 }
@@ -17,17 +16,21 @@ Task Init -depends Clean -Description "初始化参数" {
 }
 
 Task Nuget -depends Init -Description "nuget restore" {
-    dotnet restore Newbe.Mahua.sln
+    dotnet restore Newbe.Mahua.sln --packages packages
 }
 
 Task Build -depends Nuget -Description "编译所有解决方案" {
     Exec {
         msbuild /m /t:"Build" /p:Configuration=$deployMode /v:minimal /nologo  Newbe.Mahua.Native.sln
+    }
+    Exec {
         msbuild /m /t:"Build" /p:Configuration=$deployMode /v:minimal /nologo  Newbe.Mahua.sln
     }
 }
 Task Pack -depends Build -Description "打包" {
-    dotnet pack Newbe.Mahua.Pack.sln -c $deployMode --no-build
+    Exec {
+        dotnet pack Newbe.Mahua.Pack.sln -c $deployMode --no-build
+    }
 }
 Task NugetPushLocal -depends Pack -Description "推送nuget包到本地" {
     Write-Output "构建完毕，当前时间为 $(Get-Date)"
@@ -35,7 +38,9 @@ Task NugetPushLocal -depends Pack -Description "推送nuget包到本地" {
 
 Task NugetPushNuget -depends Pack -Description "推送nuget包到nuget.org" {
     Get-ChildItem $releaseDir *.nupkg | ForEach-Object {
-        cmd /c "$nugetexe push $releaseDir$_ -Source https://www.nuget.org/api/v2/package"
+        Exec {
+            dotnet nuget push "$releaseDir$_" -s https://www.nuget.org/
+        }
     }
     Write-Output "构建完毕，当前时间为 $(Get-Date)"
 }
