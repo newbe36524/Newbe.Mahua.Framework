ada62d1fe26c6a4998ca4297910c0148e0352fd4
newbe36524 <472158246@qq.com>
2019-09-21 20:10:08 +0800

    add some log for CleverQQ

--- src/Newbe.Mahua.CleverQQ/Commands/EventFunCommand.cs
+++ src/Newbe.Mahua.CleverQQ/Commands/EventFunCommand.cs
@@ -1,4 +1,5 @@
-﻿using Autofac.Features.Indexed;
+﻿using System;
+using Autofac.Features.Indexed;
 using Newbe.Mahua.Commands;
 using Newbe.Mahua.Logging;
 using System.Runtime.Serialization;
@@ -9,6 +10,12 @@ namespace Newbe.Mahua.CleverQQ.Commands
     public class EventFunCommandHandler : ICommandHandler<EventFunCommand, EventFunCommandResult>
     {
         private static readonly ILog Logger = LogProvider.For<EventFunCommandHandler>();
+
+        private static readonly JavaScriptSerializer JavaScriptSerializer = new JavaScriptSerializer
+        {
+            MaxJsonLength = int.MaxValue
+        };
+
         private readonly IRobotSessionContext _robotSessionContext;
         private readonly IIrEventOutput _eventFunOutput;
         private readonly IIndex<int, IIrEvent> _eventFuncHandlers;
@@ -42,11 +49,18 @@ namespace Newbe.Mahua.CleverQQ.Commands
             }
             else
             {
-                var js = new JavaScriptSerializer
+                object data = null;
+                try
                 {
-                    MaxJsonLength = int.MaxValue
-                };
-                Logger.Warn($"没有能够处理CleverQQ事件，事件内容为{js.Serialize(message)}");
+                    data = JavaScriptSerializer.Serialize(message);
+                }
+                catch (Exception e)
+                {
+                    Logger.WarnException("尝试将Message序列化为 Json 时遇到了异常，将降级处理", e);
+                    data = message;
+                }
+
+                Logger.Warn("没有能够处理CleverQQ事件，事件内容为{data}", data);
             }
 
             return new EventFunCommandResult
@@ -59,41 +73,30 @@ namespace Newbe.Mahua.CleverQQ.Commands
     [DataContract]
     public class EventFunCommand : CleverQQCommand<EventFunCommandResult>
     {
-        [DataMember]
-        public string ReceiverQq { get; set; }
+        [DataMember] public string ReceiverQq { get; set; }
 
-        [DataMember]
-        public int EventType { get; set; }
+        [DataMember] public int EventType { get; set; }
 
-        [DataMember]
-        public int EventAdditionType { get; set; }
+        [DataMember] public int EventAdditionType { get; set; }
 
-        [DataMember]
-        public string FromNum { get; set; }
+        [DataMember] public string FromNum { get; set; }
 
-        [DataMember]
-        public string EventOperator { get; set; }
+        [DataMember] public string EventOperator { get; set; }
 
-        [DataMember]
-        public string Triggee { get; set; }
+        [DataMember] public string Triggee { get; set; }
 
-        [DataMember]
-        public string Message { get; set; }
+        [DataMember] public string Message { get; set; }
 
-        [DataMember]
-        public string RawMessage { get; set; }
+        [DataMember] public string RawMessage { get; set; }
 
-        [DataMember]
-        public long MessageId { get; set; }
+        [DataMember] public long MessageId { get; set; }
 
-        [DataMember]
-        public long MessageNum { get; set; }
+        [DataMember] public long MessageNum { get; set; }
     }
 
     [DataContract]
     public class EventFunCommandResult : CleverQQCommandResult
     {
-        [DataMember]
-        public int Result { get; set; }
+        [DataMember] public int Result { get; set; }
     }
 }
